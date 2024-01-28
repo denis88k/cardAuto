da1522f0fff89f76a0225d876fe23bdcc772432c
denis88k <denis88.k@mail.ru>
2024-01-28 23:54:21 +0300

    mission complete ^-)

--- src/js/_components.js
+++ src/js/_components.js
@@ -4,6 +4,7 @@ import './components/headerMenu.js'; // header-menu
 import './components/like.js'; // like
 import './components/modalCredit.js'; // modalCredit
 import './components/modalDiscount.js'; // modalDiscount
+import './components/modalReport.js'; // modalReport
 import './components/slider.js'; // slider
 import './components/timer.js'; // таймер
 import './components/validateForm.js'; // validateForm
