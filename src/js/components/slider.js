e2fa86ef98a94461faa4ea25fdd472347e616d1e
denis88k <denis88.k@mail.ru>
2024-01-29 23:58:17 +0300

    правки 2

--- src/js/components/slider.js
+++ src/js/components/slider.js
@@ -1,40 +1,40 @@
-import './../vendor/swiper.js';
+// import './../vendor/swiper.js';
 
-// слайдер для карточки авто
-const cardSlider = document.querySelector('.card__swiper');
-const cardSliderMini = document.querySelector('.card__swiper-mini');
+// // слайдер для карточки авто
+// const cardSlider = document.querySelector('.card__swiper');
+// const cardSliderMini = document.querySelector('.card__swiper-mini');
 
-let swiperCardMini = new Swiper(cardSliderMini, {
-	slidesPerView: 'auto',
-	spaceBetween: 1,
-});
+// let swiperCardMini = new Swiper(cardSliderMini, {
+// 	slidesPerView: 'auto',
+// 	spaceBetween: 1,
+// });
 
-let swiperCard = new Swiper(cardSlider, {
-	centeredSlides: true,
-	slidesPerView: '1',
-	effect: 'fade',
-	thumbs: {
-		swiper: swiperCardMini,
-	},
-});
+// let swiperCard = new Swiper(cardSlider, {
+// 	centeredSlides: true,
+// 	slidesPerView: '1',
+// 	effect: 'fade',
+// 	thumbs: {
+// 		swiper: swiperCardMini,
+// 	},
+// });
 
-// слайдер для карточки с похожими авто similar car
-const cardBasic = document.querySelector('.similar_car__card-basic');
-const sliderBasic = cardBasic.querySelectorAll('.similar_car__swiper');
+// // слайдер для карточки с похожими авто similar car
+// const cardBasic = document.querySelector('.similar_car__card-basic');
+// const sliderBasic = cardBasic.querySelectorAll('.similar_car__swiper');
 
-const initSlider = slider => {
-	let swiper = new Swiper(slider, {
-		slidesPerView: 'auto',
-		spaceBetween: 1,
-		breakpoints: {
-			560: {
-				slidesPerView: '1',
-				effect: 'fade',
-			},
-		},
-	});
-};
+// const initSlider = slider => {
+// 	let swiper = new Swiper(slider, {
+// 		slidesPerView: 'auto',
+// 		spaceBetween: 1,
+// 		breakpoints: {
+// 			560: {
+// 				slidesPerView: '1',
+// 				effect: 'fade',
+// 			},
+// 		},
+// 	});
+// };
 
-sliderBasic.forEach(slider => {
-	initSlider(slider);
-});
+// sliderBasic.forEach(slider => {
+// 	initSlider(slider);
+// });
